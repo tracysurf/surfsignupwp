d8db3bc16420ffd29c463754c458087a5d4c016f
Roger Coathup <roger@21applications.com>
2017-02-07 10:13:28 +0000

    Removed restrictions on page widths

--- themes/surf-signup/template-parts/content.php
+++ themes/surf-signup/template-parts/content.php
@@ -12,10 +12,11 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php
+		printf( '<a href="%s" rel="bookmark">%s</a>', esc_url( get_permalink() ), get_the_post_thumbnail( get_the_ID(), 'large' ) );
+
 		if ( is_single() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
-			printf( '<a href="%s" rel="bookmark">%s</a>', esc_url( get_permalink() ), get_the_post_thumbnail( get_the_ID(), 'large' ) );
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
